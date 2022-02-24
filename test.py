c29dae8502d5ceb74f11880206618233278f3856
ddotz <35992522+ddotz@users.noreply.github.com>
2022-02-24 20:23:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import time
 if __name__ == '__main__':
-  print('nothing to do!')
+  print('Test Start:\n')
+  time1 = time.time()
+  time2 = time.localtime(time1)
+  time3 = time.asctime(time2)
+  print(time1 + '\n' + time2 + '\n' + time3)
   
