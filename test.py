3e23431c6709c5816ad82b3032d4255dbb43594d
ddotz <35992522+ddotz@users.noreply.github.com>
2022-02-24 20:16:21 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import time
 if __name__ == '__main__':
   time1 = time.time()
-  time2 = time.localtime(time1)
-  time3 = time.asctime(time2)
+#   time2 = time.localtime(time1)
+#   time3 = time.asctime(time2)
   
